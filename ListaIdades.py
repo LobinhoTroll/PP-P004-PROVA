f705b14f353a4c445c81abe7a70e30cdd68072ec
LobinhoTroll <gabrielteles931@gmail.com>
2023-12-04 10:53:51 -0300

    entradadedados()

--- ListaIdades.py
+++ ListaIdades.py
@@ -8,7 +8,25 @@ class ListaIdades(AnaliseDados):
         self.__lista = []        
     
     def entradaDeDados(self):
+        qtdItens,contador,idade = 0,0,0
         
+        print("Informe a quantidade de itens na lista: ")
+        qtdItens = int(input())
+
+        if qtdItens > 0:
+            while contador < qtdItens:
+                print("\nInserindo Idades na lista:")
+                
+                print("\nIdade", contador + 1, ": ")
+                
+                idade = int(input("Digite a idade: "))
+                
+                try:
+                    idade = Idade(idade)
+                    self.__lista.append(str(idade))
+                    contador += 1
+                except ValueError as e:
+                    print(f"Erro: {e}. Idade inválida. Por favor, insira uma idade válida.")
     
     def mostraMediana(self):
         
