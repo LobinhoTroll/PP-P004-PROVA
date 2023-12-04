8e47768ad11681d5c7445fe5387272afb592b267
LobinhoTroll <gabrielteles931@gmail.com>
2023-12-04 10:43:51 -0300

    Class ListaIdades

---
+++ ListaIdades.py
@@ -0,0 +1,23 @@
+from AnaliseDados import AnaliseDados
+from Idade import Idade
+
+class ListaIdades(AnaliseDados):
+    
+    def __init__(self):
+        super().__init__(type(Idade))
+        self.__lista = []        
+    
+    def entradaDeDados(self):
+        
+    
+    def mostraMediana(self):
+        
+
+    def mostraMenor(self):
+        
+    
+    def mostraMaior(self):
+        
+
+    def __str__(self):
+        pass
